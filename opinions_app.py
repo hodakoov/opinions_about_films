aee0fcda6c3138121b56daeba99303c1cb6c8a77
Khodakov Valery <teeror93@gmail.com>
2024-01-09 00:50:18 +0400

    обработка 404 и 500 ошибки. собственные шаблоны

--- opinions_app.py
+++ opinions_app.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -46,7 +46,7 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных мнений о фильмах нет.'
+        abort(404)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template('opinion.html', opinion=opinion)
@@ -75,6 +75,19 @@ def add_opinion_view():
 def opinion_view(id: int):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # В качестве ответа возвращается собственный шаблон и код ошибки
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    # В таких случаях можно откатить незафиксированные изменения в БД
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
