c626e16cedfac833ba347117bab13a9dcc74b2f9
Khodakov Valery <teeror93@gmail.com>
2024-01-09 01:05:18 +0400

    Обновление с бд. Работа с миграциями. 1) 1 инициализация - flask db init 2) Создание миграции - flask db migrate -m "added added_by field" 3) Применение миграций -  flask db upgrade

--- opinions_app.py
+++ opinions_app.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -23,6 +25,7 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    added_by = db.Column(db.String(64))
 
 
 class OpinionForm(FlaskForm):
